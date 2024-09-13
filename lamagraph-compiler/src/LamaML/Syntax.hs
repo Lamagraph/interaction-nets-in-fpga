431427a1a1f89cbe8bcfe0d463108b9fbbb3908b
Nikolai Ponomarev <wowasterdev@gmail.com>
2024-09-13 16:39:08 +0300

    Add remaining parts of syntax

--- lamagraph-compiler/src/LamaML/Syntax.hs
+++ lamagraph-compiler/src/LamaML/Syntax.hs
@@ -31,13 +31,35 @@ module LamaML.Syntax (
 
     -- ** Keywords
     -- $lexing_keywords
+
+    -- * Names
+    -- $names
+
+    -- * Type expressions
+    -- $types
+
+    -- * Constants
+    -- $constants
+
+    -- * Patterns
+    -- $patterns
+
+    -- * Expressions
+    -- $expressions
+
+    -- * Type definitions
+    -- $typedefs
+
+    -- * Declarations
+    -- $decls
 ) where
 
 {- $rules
 Grammar rules are written in monospace font.
-Upright words represent @nonterminals@.
-@\"Terminals"@ are written in quotes.
+Italicized words represent @/nonterminals/@.
+@Terminals@ are written in upright font.
 @( )@ mean grouping, @{ }@ mean zero or more repetitions, @[ ]@ mean optional.
+In case of clash between grammar and terminal meaning, terminal symbol will be typeset as @__bold monospace__@.
 -}
 
 {- $lexing_blanks
@@ -51,19 +73,19 @@ Nesting is allowed and must be handled.
 
 {- $lexing_idents
 @
-ident ::= (letter | "_") {letter | "0"..."9" | "_" | "'"}
+/ident/ ::= ( /letter/ | _ ) { /letter/ | 0...9 | _ | ' }
 
-capitalized-ident ::= (\"A"...\"Z") {letter | "0"..."9" | "_" | "'"}
+/capitalized-ident/ ::= ( A...Z ) { /letter/ | 0...9 | _ | ' }
 
-lowercase-ident ::= ("a"..."z" | "_") {letter | "0"..."9" | "_" | "'"}
+/lowercase-ident/ ::= ( a...z | _ ) { /letter/ | 0...9 | _ | ' }
 
-letter ::= \"A"...\"Z" | "a"..."z"
+/letter/ ::= A...Z | a...z
 @
 -}
 
 {- $lexing_int_lits
 @
-integer-literal ::= \["-"] ("0"..."9") {"0"..."9" | "_"}
+/integer-literal/ ::= \[-] ( 0...9 ) { 0...9 | _ }
 @
 
 /Do we need hex\/octal\/binary numbers and\/or size suffixes?/
@@ -73,10 +95,10 @@ integer-literal ::= \["-"] ("0"..."9") {"0"..."9" | "_"}
 
 {- $lexing_char_lits
 @
-char-literal ::= "'" regular-char "'"
-               | "'" escape-sequence "'"
+/char-literal/ ::= ' /regular-char/ '
+               | ' /escape-sequence/ '
 
-escape-sequence ::= "\" ("|" | """ | "'" | "n")
+/escape-sequence/ ::= \\ ( __|__ | " | ' | n )
 @
 
 /Do we need more escape sequences?/
@@ -86,10 +108,10 @@ escape-sequence ::= "\" ("|" | """ | "'" | "n")
 
 {- $lexing_string_lits
 @
-string-literal ::= """ { string-character } """
+/string-literal/ ::= " { /string-character/ } "
 
-string-character ::= regular-string-character
-                   | escape-sequence
+/string-character/ ::= /regular-string-character/
+                   | /escape-sequence/
 @
 
 /We must define regular-string-character? Any ASCII one?/
@@ -98,9 +120,171 @@ string-character ::= regular-string-character
 -}
 
 {- $lexing_operators
-revise later
+
+@
+/infix-symbol/ ::= ( = | \< | \> | @ | ^ | __|__ | & | + | - | * | / | $ | % ) { /operator-char/ }
+
+/prefix-symbol/ ::= ! { /operator-char/ }
+                | ( ? | ~ ) { /operator-char/ }+
+
+/operator-char/ ::= ! | $ | % | & | * | + | - | . | / | : | \< | = | \> | ? | @ | ^ | __|__ | ~
+@
+
+Copypasted from https://askra.de/software/ocaml-doc/4.02/lex.html#sec71, probably too complicated.
 -}
 
 {- $lexing_keywords
-revise later
+Keyword table:
+
+@
+and asr begin else end false fun function
+if in land let lor lsl lsr lxor match mod
+of rec then true type when with
+
+!= && ' ( ) * + , - -> : :: ; < = > [ ]
+_ { | }
+@
+
+/I hope nothing is missing./
+-}
+
+{- $names
+/No modules currently!/
+
+@
+/value-name/ ::= /lowercase-ident/
+             | __(__ /operator-name/ __)__
+
+/operator-name/ ::= /prefix-symbol/ | /infix-op/
+
+/infix-op/ ::= /infix-symbol/
+           | * | + | - | = | != | \< | \> | || | &&
+           | mod | land | lor | lxor | lsl | lsr | asr
+
+/constr-name/ ::= /capitalized-ident/
+
+/typeconstr-name/ ::= /lowercase-ident/
+
+/field-name/ ::= /lowercase-ident/
+@
+
+/Revise or \/ || \/ lor/
+
+/Do we need records?/
+-}
+
+{- $types
+@
+/typexpr/ ::= ' /ident/
+          | _
+          | __(__ /typexpr/ __)__
+          | /typexpr/ -> /typexpr/
+          | /typexpr/ { * /typexpr/}+
+          | /typeconstr-name/
+          | /typexpr/ /typeconstr-name/
+          | __(__ /typexpr/ {, /typexpr/} __)__ /typeconstr-name/
+@
+-}
+
+{- $constants
+@
+/constant/ ::= /integer-literal/
+           | /char-literal/
+           | /string-literal/
+           | /constr-name/
+           | false
+           | true
+           | __()__
+           | begin end
+           | __[]__
+@
+-}
+
+{- $patterns
+@
+/pattern/ ::= /value-name/
+          | _
+          | /constant/
+          | __(__ /pattern/ __)__
+          | __(__ /pattern/ : /typexpr/ __)__
+          | /pattern/ __|__ /pattern/
+          | /constr-name/ /pattern/
+          | /pattern/ { , /pattern/ }
+          | __{__ /field-name/ [: /typexpr/] [= /pattern/] { ; /field-name/ [: /typexpr/] [= /pattern/] } [; _] [;] __}__
+          | __[__ /pattern/ { ; /pattern/ } [;] __]__
+          | /pattern/ :: /pattern/
+@
+-}
+
+{- $expressions
+
+@
+/expr/ ::= /value-name/
+       | /constant/
+       | __(__ /expr/ __)__
+       | begin /expr/ end
+       | /expr/ {, /expr/ }
+       | /constr-name/ /expr/
+       | /expr/ :: /expr/
+       | __[__ /expr/ { ; /expr/ } [;] ]
+       | __{__ /field-name/ [: /typexpr/] [= /expr/] { ; /field-name/ [: /typexpr/] [= /expr/] } [;] __}__
+       | __{__ /expr/ with /field-name/ [: /typexpr/] [= /expr/] { ; /field-name/ [: /typexpr/] [= /expr/] } [;] __}__
+       | /expr/ { /argument/ }+
+       | /prefix-symbol/ /expr/
+       | - /expr/
+       | /expr/ /infix-op/ /expr/
+       | if /expr/ then /expr/ else /expr/
+       | match /expr/ with /pattern-matching/
+       | function /pattern-matching/
+       | fun { /parameter/ }+ [ : /typexpr/ ] -> /expr/
+       | let [rec] /let-binding/ { and /let-binding/ } in /expr/
+
+/argument/ ::= /expr/
+
+/pattern-matching/ ::= [ __|__ ] /pattern/ [ when /expr/ ] -> /expr/ { __|__ /pattern/ [ when /expr/ ] -> /expr/ }
+
+/let-binding/ ::= /pattern/ = /expr/
+              | /value-name/ { /parameter/ } [ : /typexpr/ ] = /expr/
+
+/parameter/ ::= /pattern/
+@
+-}
+
+{- $typedefs
+
+@
+/type-definition/ ::= type /typedef/ { and /typedef/ }
+
+/typedef/ ::= [ /type-params/ ] /typeconstr-name/ /type-information/
+
+/type-information/ ::= [ /type-equation/ ] [ /type-representation/ ]
+
+/type-equation/ ::= = /typexpr/
+
+/type-representation/ ::= = [ __|__ ] /constr-decl/ { __|__ /constr-decl/ }
+                      | = /record=decl/
+                      | = __|__
+
+/type-params/ ::= /type-param/
+              | __(__ /type-param/ {, /type-param/ __)__
+
+/type-param/ ::= ' /ident/
+
+/record-decl/ ::= __{__ /field-decl/ { ; /field-decl/ } [;] __}__
+
+/constr-decl/ ::= ( /constr-name/ | __[]__ | __(__::__)__ ) [ of /constr-args/ ]
+
+/constr-args/ ::= /typexpr/ { * /typexpr/ }
+
+/field-decl/ ::= /field-name/ : /typexpr/
+@
+-}
+
+{- $decls
+
+@
+/decl/ ::= /expr/ | /type-definition/
+
+/prog/ ::= { /decl/ }
+@
 -}
