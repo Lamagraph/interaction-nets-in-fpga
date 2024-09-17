c42efc49666c4c22dd010f8a4136e519f245a818
Efim Kubishkin <efimkub@mail.ru>
2024-09-17 09:45:06 +0300

    Add tests for selectAddresToLoad function

--- lamagraph-core/tests/Tests/Core/Node.hs
+++ lamagraph-core/tests/Tests/Core/Node.hs
@@ -1,16 +1,18 @@
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# OPTIONS_GHC -Wno-partial-type-signatures #-}
+
 module Tests.Core.Node where
 
 import Prelude
-
-import Test.Tasty
-import Test.Tasty.Hedgehog
-import Test.Tasty.TH
 
 import qualified Clash.Prelude as C
 import Core.Node
 import qualified Hedgehog as H
 import qualified Hedgehog.Gen as Gen
-import NodeGenerate (genAddress)
+import NodeGenerate (genAddress, genMbObjectsVec, genPortVisitedFlag)
+import Test.Tasty
+import Test.Tasty.Hedgehog
+import Test.Tasty.TH
 
 prop_isPortToLoad_diffAddr :: H.Property
 prop_isPortToLoad_diffAddr = H.property $ do
@@ -62,6 +64,28 @@ prop_isActive_random = H.property $ do
     rightNode = Node rightPrimPort C.Nil
     rightLoadedNode = LoadedNode rightNode rightAddress
   isActive leftLoadedNode rightLoadedNode H.=== (leftAddress == rightPortAddr && rightAddress == leftPortAddr)
+
+prop_selectAddressToLoad_primary_unvisited :: H.Property
+prop_selectAddressToLoad_primary_unvisited = H.property $ do
+  nodeAddress <- H.forAll genAddress
+  portTargetAddress <- H.forAll genAddress
+  let
+    port = Port portTargetAddress False
+    node = Node port C.Nil
+    loadedNode = LoadedNode node nodeAddress
+  case selectAddressToLoad loadedNode of
+    Nothing -> nodeAddress H.=== portTargetAddress
+    Just address -> address H.=== portTargetAddress
+
+prop_selectAddressToLoad_all_visited :: H.Property
+prop_selectAddressToLoad_all_visited = H.property $ do
+  primPort <- H.forAll $ genPortVisitedFlag True
+  secondaryPortsVec <- H.forAll $ genMbObjectsVec (genPortVisitedFlag True) :: _ (C.Vec 10 _) -- 10 is random number, it can be changed
+  address <- H.forAll genAddress
+  let
+    node = Node primPort secondaryPortsVec
+    loadedNode = LoadedNode node address
+  selectAddressToLoad loadedNode H.=== Nothing
 
 accumTests :: TestTree
 accumTests = $(testGroupGenerator)
