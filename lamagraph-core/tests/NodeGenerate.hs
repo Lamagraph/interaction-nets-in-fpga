246205d1ceed0634cd7108ac14cf010118946744
Efim Kubishkin <efimkub@mail.ru>
2024-09-17 09:45:06 +0300

    Add tests for selectAddresToLoad function

--- lamagraph-core/tests/NodeGenerate.hs
+++ lamagraph-core/tests/NodeGenerate.hs
@@ -21,16 +21,24 @@ genPort = do
   address <- genAddress
   Port address <$> Gen.bool
 
+genPortVisitedFlag :: (MonadGen m) => Bool -> m Port
+genPortVisitedFlag visited = do
+  address <- genAddress
+  return $ Port address visited
+
 type GenNode (n :: Nat) = forall m. (MonadGen m) => m (Node n)
 
 genNode :: forall m n. (MonadGen m, KnownNat n) => m (Node n)
 genNode = do
   port <- genPort
-  ports <- genVec genMbPort :: m (Vec n _)
+  ports <- genMbObjectsVec genPort :: _ (Vec n _)
   return $ Node port ports
+
+genMbObjectsVec :: forall m n a. (MonadGen m, KnownNat n) => m a -> m (Vec n (Maybe a))
+genMbObjectsVec genObjectFunc = genVec genFunc
  where
-  genMbPort =
+  genFunc =
     Gen.frequency
-      [ (70, Just <$> genPort)
+      [ (70, Just <$> genObjectFunc)
       , (30, return Nothing)
       ]
