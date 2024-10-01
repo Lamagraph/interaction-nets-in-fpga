8e4d5789afd2e177b395eabe0e806b4472ea867e
Efim Kubishkin <efimkub@mail.ru>
2024-10-01 09:48:49 +0300

    Add updateRam function

--- lamagraph-core/src/Core/Loader.hs
+++ lamagraph-core/src/Core/Loader.hs
@@ -3,19 +3,36 @@ module Core.Loader where
 import Clash.Prelude
 import Core.Node
 
--- | Get `Node` by his `Address` from RAM. Actually, preparing to reducer work.
+-- | Get `Node` by his `AddressNumber` from RAM. Actually, preparing to reducer work.
 loader ::
   ( KnownDomain dom
   , HiddenClockResetEnable dom
   , KnownNat numberOfPorts
   ) =>
-  (Signal dom Address -> Signal dom (Node numberOfPorts)) ->
-  Signal dom (Maybe Address) ->
+  (Signal dom AddressNumber -> Signal dom (Node numberOfPorts)) ->
+  Signal dom (Maybe AddressNumber) ->
   Signal dom (Maybe (LoadedNode numberOfPorts))
-loader ram mbAddressToLoad =
-  mkLoadedNode <$> mbNode <*> mbAddressToLoad
+loader ram mbAddressNumberToLoad =
+  mkLoadedNode <$> mbNode <*> mbAddressNumberToLoad
  where
-  mkLoadedNode node address = LoadedNode <$> node <*> address
-  mbNode = case sequenceA mbAddressToLoad of
+  mkLoadedNode node address = LoadedNode <$> node <*> (ActualAddress <$> address)
+  mbNode = case sequenceA mbAddressNumberToLoad of
     Nothing -> pure Nothing
     Just n -> sequenceA $ Just (ram n)
+
+{- | Update RAM function. In fact only way to update data in registers.
+Change `Node` by given `AddressNumber` at given `Node`
+-}
+updateRam ::
+  ( KnownDomain dom
+  , HiddenClockResetEnable dom
+  , KnownNat numberOfPorts
+  ) =>
+  (Signal dom AddressNumber -> Signal dom (Node numberOfPorts)) ->
+  Signal dom AddressNumber ->
+  Signal dom (Node numberOfPorts) ->
+  (Signal dom AddressNumber -> Signal dom (Node numberOfPorts))
+updateRam oldRam newAddressNumber newNode address = mux addressesIsEq newNode oldNode
+ where
+  addressesIsEq = newAddressNumber .==. address
+  oldNode = oldRam address
