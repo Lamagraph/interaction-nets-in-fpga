7f3e3996a235ba36930e1a6ee48ddf41306b0def
Efim Kubishkin <efimkub@mail.ru>
2024-11-08 10:11:38 +0300

    Rename Map functions

--- lamagraph-core/src/Core/Map.hs
+++ lamagraph-core/src/Core/Map.hs
@@ -1,4 +1,4 @@
-module Core.Map (Map, findByKey, changeValueByKey) where
+module Core.Map (Map, find, insertWith) where
 
 import Clash.Prelude
 import Core.Node
@@ -9,48 +9,48 @@ It assumed that `size` is small
 -}
 type Map (size :: Nat) v = (Vec size (Maybe (AddressNumber, Maybe v)))
 
-findByKey ::
+find ::
   (KnownNat size) =>
   Map size v ->
   AddressNumber ->
   Maybe v
-findByKey dict key = case dict of
+find dict key = case dict of
   Nil -> Nothing
-  (Just (k, v) `Cons` t) -> if k == key then v else findByKey t key
-  (Nothing `Cons` t) -> findByKey t key
+  (Just (k, v) `Cons` t) -> if k == key then v else find t key
+  (Nothing `Cons` t) -> find t key
 
 -- | Update or insert (by applying function to `Nothing`) value by the key
-changeValueByKey ::
+insertWith ::
   (KnownNat size, Eq v) =>
   Map size v ->
   (Maybe v -> Maybe v) ->
   AddressNumber ->
   Map size v
-changeValueByKey dict func key = if dict /= updated then updated else inserted
+insertWith dict func key = if dict /= updated then updated else inserted
  where
-  updated = tryToUpdate key func dict
-  inserted = insertInFree key (func Nothing) dict
+  updated = update key func dict
+  inserted = insert key (func Nothing) dict
 
--- | Try to update (by applying the function) value by key. It do nothing if there is no value be the key in the `Map`
-tryToUpdate ::
+-- | Try to update (by applying the function) value by key. It does nothing if there is no value by the key in the `Map`
+update ::
   AddressNumber ->
   (Maybe v -> Maybe v) ->
   Map size v ->
   Map size v
-tryToUpdate key func dict = case dict of
+update key func dict = case dict of
   h `Cons` t -> case h of
-    Nothing -> h `Cons` tryToUpdate key func t
-    Just (k, v) -> if k == key then Just (k, func v) `Cons` t else h `Cons` tryToUpdate key func t
+    Nothing -> h `Cons` update key func t
+    Just (k, v) -> if k == key then Just (k, func v) `Cons` t else h `Cons` update key func t
   Nil -> Nil
 
 -- | Insert key-value pair in the free space
-insertInFree ::
+insert ::
   AddressNumber ->
   Maybe v ->
   Map size v ->
   Map size v
-insertInFree key value dict = case dict of
+insert key value dict = case dict of
   h `Cons` t -> case h of
     Nothing -> Just (key, value) `Cons` t
-    Just _ -> h `Cons` insertInFree key value t
+    Just _ -> h `Cons` insert key value t
   Nil -> error "All addresses are written"
