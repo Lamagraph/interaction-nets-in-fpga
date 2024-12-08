3262a0455fbcbef8345e2eea4b76f4d5e904bbdb
Efim Kubishkin <efimkub@mail.ru>
2024-12-08 13:07:21 +0300

    Replace Edge, ActivePair and Delta type

--- lamagraph-core/src/Core/Reducer.hs
+++ lamagraph-core/src/Core/Reducer.hs
@@ -1,24 +1,49 @@
-{-# HLINT ignore "Functor law" #-}
--- {-# LANGUAGE PartialTypeSignatures #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Functor law" #-}
 
 module Core.Reducer where
 
 import Clash.Prelude
-import Control.Lens (makeLenses, view, (^.))
-import Core.MemoryManager
+import Control.Lens
 import Core.Node
 
 type NumOfNodesToStore = Unsigned 3
 type NumOfEdgesToStore = Unsigned 3
 
+data Edge (portsNumber :: Nat) = Edge
+  { _leftEnd :: Connection portsNumber
+  , _rightEnd :: Connection portsNumber
+  }
+  deriving (Generic, NFDataX, Show, Eq)
+
+$(makeLenses ''Edge)
+
+data ActivePair (portsNumber :: Nat) = ActivePair
+  { _leftNode :: LoadedNode portsNumber
+  , _rightNode :: LoadedNode portsNumber
+  }
+  deriving (Show, Eq, Generic, NFDataX, Bundle)
+$(makeLenses ''ActivePair)
+
 -- | Result of abstract reduction rule
 data ReduceRuleResult (nodesNumber :: Nat) (edgesNumber :: Nat) (portsNumber :: Nat) = ReduceRuleResult
   { _edges :: Vec edgesNumber (Maybe (Edge portsNumber))
-  , _nodes :: Vec nodesNumber (Maybe (LocalNode portsNumber))
+  , _nodes :: Vec nodesNumber (Maybe (LoadedNode portsNumber))
   }
 
 $(makeLenses ''ReduceRuleResult)
+
+data Delta (nodesNumber :: Nat) (edgesNumber :: Nat) (portsNumber :: Nat) = Delta
+  { _newNodes :: Vec nodesNumber (Maybe (LoadedNode portsNumber))
+  , _newEdges :: Vec edgesNumber (Maybe (Edge portsNumber))
+  , _activePair :: ActivePair portsNumber
+  }
+  deriving (Show, Eq, Generic, NFDataX)
+
+$(makeLenses ''Delta)
+
+type Interface externalNodesNumber = Vec externalNodesNumber (Maybe AddressNumber)
 
 toDelta ::
   (KnownNat portsNumber, KnownNat edgesNumber, KnownNat nodesNumber) =>
