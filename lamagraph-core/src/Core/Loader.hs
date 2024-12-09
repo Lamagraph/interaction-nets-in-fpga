01db43eef80c167cf79cd15366ed7e66c68bad47
Efim Kubishkin <efimkub@mail.ru>
2024-12-09 12:48:29 +0300

    Add necessary loading functions

--- lamagraph-core/src/Core/Loader.hs
+++ lamagraph-core/src/Core/Loader.hs
@@ -1,22 +1,57 @@
 module Core.Loader where
 
 import Clash.Prelude
+import Control.Lens (view)
+import Core.MemoryManager.MemoryManager (ActivePair (ActivePair))
 import Core.MemoryManager.NodeChanges
 import Core.Node
 
--- | Get `Node` by his `AddressNumber` from RAM. Actually, preparing to reducer work.
-loader ::
+-- | Type alias for partial applied `blockRam`
+type Ram dom portsNumber =
+  ( Signal dom AddressNumber ->
+    Signal dom (Maybe (AddressNumber, Maybe (Node portsNumber))) ->
+    Signal dom (Maybe (Node portsNumber))
+  )
+
+-- | Read external `Node`s from ram
+loadInterface ::
   ( KnownDomain dom
   , HiddenClockResetEnable dom
-  , KnownNat numberOfPorts
+  , KnownNat portsNumber
+  , KnownNat externalNodesNumber
   ) =>
-  (Signal dom AddressNumber -> Signal dom (Node numberOfPorts)) ->
-  Signal dom (Maybe AddressNumber) ->
-  Signal dom (Maybe (LoadedNode numberOfPorts))
-loader ram mbAddressNumberToLoad =
-  mkLoadedNode <$> mbNode <*> mbAddressNumberToLoad
+  Ram dom portsNumber ->
+  Signal dom (Interface externalNodesNumber) ->
+  Signal dom (Vec externalNodesNumber (Maybe (LoadedNode portsNumber)))
+loadInterface ram interface =
+  bundle $
+    map
+      (traverse readFromRam . sequenceA)
+      (unbundle interface)
  where
-  mkLoadedNode node address = LoadedNode <$> node <*> address
-  mbNode = case sequenceA mbAddressNumberToLoad of
-    Nothing -> pure Nothing
-    Just n -> sequenceA $ Just (ram n)
+  partRam address = ram address def
+  readFromRam address = case sequenceA (partRam address) of
+    Just node -> LoadedNode <$> node <*> address
+    Nothing -> error "An attempt to read at a free address"
+
+-- | Load `ActivePair` by `AddressNumber`. It is assumed that `AddressNumber` is actually active
+loadActivePair ::
+  (KnownDomain dom, HiddenClockResetEnable dom, KnownNat portsNumber) =>
+  Ram dom portsNumber ->
+  Signal dom AddressNumber ->
+  Signal dom (ActivePair portsNumber)
+loadActivePair ram leftActiveNodeAddress =
+  ActivePair
+    <$> (LoadedNode <$> leftActiveNode <*> leftActiveNodeAddress)
+    <*> (LoadedNode <$> rightActiveNode <*> rightActiveNodeAddress)
+ where
+  partRam address = ram address def
+  getNodeByAddress address = case sequenceA $ partRam address of
+    Just node -> node
+    Nothing -> error "An attempt to read at a free address"
+  getRightActiveNodeAddress node = case view primaryPort node of
+    Connected port -> view nodeAddress port
+    NotConnected -> error "Wrong definition of active pair"
+  leftActiveNode = getNodeByAddress leftActiveNodeAddress
+  rightActiveNodeAddress = getRightActiveNodeAddress <$> leftActiveNode
+  rightActiveNode = getNodeByAddress rightActiveNodeAddress
