e7cbadf74c4f69645db94070664fefcc2ec06af1
Nick Ponomarev <wowasterdev@gmail.com>
2025-05-21 20:38:40 +0300

    Add statistics about parallel reductions

--- lamagraph-compiler/src/Lamagraph/Compiler/Nets/Reduction.hs
+++ lamagraph-compiler/src/Lamagraph/Compiler/Nets/Reduction.hs
@@ -63,23 +63,31 @@ reduceStep rule conf@Configuration{stack, cycles, threadState} = case threadStat
   -- T.1
   Delist -> case stack of
     [] -> throwIO $ DelistFromEmptyStackException conf
-    hd : tl -> pure $ conf{stack = tl, threadState = Process hd}
-  -- T.2
-  Enlist (hd : tl) -> pure $ conf{stack = hd : stack, threadState = Enlist tl}
-  -- T.3
-  Enlist [] -> pure $ conf{threadState = Delist}
+    [Nothing] -> throwIO $ DelistFromEmptyStackException conf
+    hd : tl -> case hd of
+      Just pair -> pure $ conf{stack = tl, threadState = Process pair}
+      Nothing -> do
+        env <- ask
+        atomically $ do
+          currentReductionCounter <- readTVar $ getReductionCounter env
+          prevReductionCounter <- readTVar $ getPreviousReductionCounter env
+          modifyTVar' (getReductionWidthHistory env) ((currentReductionCounter - prevReductionCounter) :)
+          writeTVar (getPreviousReductionCounter env) currentReductionCounter
+        pure $ conf{stack = tl <> [Nothing]}
+  -- T.2 + T.3
+  Enlist pairs -> pure $ conf{stack = stack <> (Just <$> pairs), threadState = Delist}
   -- T.4
   Cycle newCycle -> pure $ conf{cycles = uncurry Map.insert newCycle cycles, threadState = Delist}
   -- Main processing
   Process pair -> process rule conf pair
 
--- | Looping version of 'reduceStep'. Works until 'stack' is empty and 'threadState' is 'Delist'.
+-- | Looping version of 'reduceStep'. Works until 'stack' is empty (actually contains only 'Nothing') and 'threadState' is 'Delist'.
 reduce ::
   (Show label, Typeable label) => Rule label -> Configuration label -> INsMachine (Configuration label)
 reduce rule = loopM helper
  where
   helper conf@Configuration{stack, threadState} = case (stack, threadState) of
-    ([], Delist) -> pure $ Right conf
+    ([Nothing], Delist) -> pure $ Right conf
     _ -> do
       newConf <- reduceStep rule conf
       pure $ Left newConf
