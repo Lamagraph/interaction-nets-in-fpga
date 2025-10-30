7ac85e72f30d8e7a4aa1e41de8dadee639c92778
Danil-Zaripov <catharsiandev@gmail.com>
2025-10-30 19:20:13 +0300

    Code cleanup

--- lamagraph-compiler/src/Lamagraph/Compiler/ModuleResolver/Program.hs
+++ lamagraph-compiler/src/Lamagraph/Compiler/ModuleResolver/Program.hs
@@ -1,18 +1,5 @@
-module Lamagraph.Compiler.ModuleResolver.Program where
+module Lamagraph.Compiler.ModuleResolver.Program (LmlProgram (..)) where
 
-import Data.HashMap.Strict qualified as HashMap
-import Lamagraph.Compiler.Core
-import Lamagraph.Compiler.Core.LmlToCore
-import Lamagraph.Compiler.Core.MonadDesugar
-import Lamagraph.Compiler.Eval
-import Lamagraph.Compiler.Extension
-import Lamagraph.Compiler.Parser
-import Lamagraph.Compiler.Parser.SrcLoc
 import Lamagraph.Compiler.Syntax
-import Lamagraph.Compiler.Syntax.Longident
-import Lamagraph.Compiler.Typechecker.DefaultEnv
-import Lamagraph.Compiler.Typechecker.Infer
-import Lamagraph.Compiler.Typechecker.TcTypes
-import Relude
 
 newtype LmlProgram pass = LmlProgram [LmlModule pass]
