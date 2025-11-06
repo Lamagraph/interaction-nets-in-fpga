daeefd73b250e182db14257168cc401974b36413
Nick Ponomarev <wowasterdev@gmail.com>
2025-11-06 12:34:46 +0300

    wip

--- lamagraph-compiler/app/Term.hs
+++ lamagraph-compiler/app/Term.hs
@@ -1,8 +1,8 @@
-module Term where
+module Term (Expr (..), skk) where
 
 import Relude
 
 data Expr = A Expr Expr | S | K | I | V String deriving (Ord, Eq, Show)
 
 skk :: Expr
-skk = A (A S K) K
+skk = flip A (V "x") $ A (A S K) K
